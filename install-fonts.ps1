722ecf064717b42edcdb29fb7262dd8e1bdb2725
Devin Sackett <dsac@hotmail.com>
2022-03-16 08:28:50 -0700

    Added Caskaydia Cove fonts

--- install-fonts.ps1
+++ install-fonts.ps1
@@ -1,8 +1,11 @@
 echo "Install fonts"
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
-foreach ($file in gci *.ttf)
+foreach ($file in gci -Recurse *.ttf)
 {
     $fileName = $file.Name
+
+    echo "Installing $fileName"
+
     if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
         echo $fileName
         dir $file | %{ $fonts.CopyHere($_.fullname) }
