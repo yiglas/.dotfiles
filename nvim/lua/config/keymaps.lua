9bd41bdd8871307390790472ad46c5516cd6b1a9
Devin Sackett <devinsackett@Devins-MacBook-Pro.local>
2025-03-14 07:52:45 -0700

    Update more vscode keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,8 @@
 
 local opts = { noremap = true, silent = true }
 
+vim.keymap.set({ "x", "v" }, "p", "P", opts)
+
 if vim.g.vscode then
   local vscode = require("vscode")
 
@@ -31,25 +33,21 @@ if vim.g.vscode then
   -- editorTextFocus && neovim.ctrlKeysNormal.s && neovim.init && neovim.mode != 'insert' && editorLangId not in 'neovim.editorLangIdExclusions'
   -- vscode-neovim.send
 
-  -- save
   -- move line up
   -- move line down
   -- open command pallet
-  -- open magit status
   -- set x to undo checkout in magit
   -- set k to move to next line in magit
   -- correct terminal space
   -- close all editors
-  -- open explorer <leader>-e
   -- in explorer on enter close explorer
   -- close explorer is open and pressed <leader>-e
   -- list projects
   -- in explorer when pressing h show/hide hidden files
   -- search in all files <leader>-s {something else}
-  -- fix gcc & gc0
 
   vim.keymap.set("n", "<leader>gg", action("magit.status"), opts)
-  vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", print_test, opts)
+  -- vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", print_test, opts)
   vim.keymap.set("n", "<leader>K", print_test, opts) -- TODO should send Keywordprg
   vim.keymap.set({ "n", "i", "v" }, "<A-j>", action("editor.action.moveLinesDownAction"), opts)
   vim.keymap.set({ "n", "i", "v" }, "<A-k>", action("editor.action.moveLinesUpAction"), opts)
@@ -57,7 +55,45 @@ if vim.g.vscode then
 
   vim.keymap.set({ "n", "v" }, "gcc", action("editor.action.commentLine"), opts)
 
+  vim.keymap.set("n", "gco", function()
+    vscode.action("editor.action.insertLineAfter")
+    vscode.action("editor.action.commentLine")
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("i", true, false, true), "n", false)
+  end, opts)
+
+  vim.keymap.set("n", "gcO", function()
+    vscode.action("editor.action.insertLineBefore")
+    vscode.action("editor.action.commentLine")
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("i", true, false, true), "n", false)
+  end, opts)
+
   vim.keymap.set({ "n", "v" }, "<leader>sg", action("search.action.openNewEditor"), opts)
+  vim.keymap.set("n", "<leader>fn", action("workbench.action.files.newUntitledFile"), opts)
+  vim.keymap.set("n", "<leader>bd", action("workbench.action.closeActiveEditor"), opts)
+
+  vim.keymap.set("n", "<leader>fp", action("workbench.action.openRecent"), opts)
+
+  vim.keymap.set({ "n", "v" }, "<leader>ca", action("editor.action.quickFix"), opts)
+
+  vim.keymap.set("n", "<leader>cr", action("editor.action.rename"), opts)
+
+  vim.keymap.set("n", "<leader>tl", action("testing.reRunLastRun"), opts)
+  vim.keymap.set("n", "<leader>to", action("testing.showMostRecentOutput"), opts)
+  vim.keymap.set("n", "<leader>tO", action("testing.openOutputPeek"), opts)
+  vim.keymap.set("n", "<leader>tR", action("testing.refreshTests"), opts)
+  vim.keymap.set("n", "<leader>tr", action("testing.runAtCursor"), opts)
+  vim.keymap.set("n", "<leader>ts", action("workbench.view.testing.focus"), opts)
+  vim.keymap.set("n", "<leader>tt", action("testing.runCurrentFile"), opts)
+  vim.keymap.set("n", "<leader>tT", action("testing.runAll"), opts)
+  vim.keymap.set("n", "<leader>td", action("testing.debugAtCursor"), opts)
+
+  vim.keymap.set("n", "<leader>db", action("editor.debug.action.toggleBreakpoint"), opts)
+  vim.keymap.set("n", "<leader>dc", action("workbench.action.debug.continue"), opts)
+  vim.keymap.set("n", "<leader>dC", action("editor.debug.action.runToCursor"), opts)
+  vim.keymap.set("n", "<leader>di", action("workbench.action.debug.stepInto"), opts)
+  vim.keymap.set("n", "<leader>do", action("workbench.action.debug.stepOut"), opts)
+  vim.keymap.set("n", "<leader>dO", action("workbench.action.debug.stepOver"), opts)
+  vim.keymap.set("n", "<leader>dP", action("workbench.action.debug.pause"), opts)
 
   return
 end
