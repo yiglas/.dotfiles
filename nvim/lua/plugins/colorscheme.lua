d5d5de8dd131b2f2cc460d6da27594a8abd1671e
Devin Sackett <devinsackett@Devins-MacBook-Pro.local>
2025-03-19 15:47:53 -0700

    Add some additional configuration for nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,9 +4,18 @@ return {
   priority = 1000,
   config = function()
     -- Example config in lua
-    vim.g.nord_contrast = true
-    vim.g.nord_borders = false
+    -- vim.g.nord_contrast = true
+    -- vim.g.nord_borders = false
+    -- vim.g.nord_disable_background = true
+    -- vim.g.nord_italic = false
+    -- vim.g.nord_uniform_diff_background = true
+    -- vim.g.nord_bold = false
+
+    vim.g.nord_contrast = false
+    vim.g.nord_borders = true
     vim.g.nord_disable_background = true
+    vim.g.nord_cursorline_transparent = true
+    vim.g.nord_enable_sidebar_background = false
     vim.g.nord_italic = false
     vim.g.nord_uniform_diff_background = true
     vim.g.nord_bold = false
