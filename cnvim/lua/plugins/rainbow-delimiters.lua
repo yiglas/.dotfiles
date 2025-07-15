5334b2a5cd52704aea7d8db1634c748bb1441d94
Devin Sackett <devinsackett@filevine.com>
2025-07-15 13:57:30 -0700

    Delay rainbow-delimiters to BufReadPre

--- cnvim/lua/plugins/rainbow-delimiters.lua
+++ cnvim/lua/plugins/rainbow-delimiters.lua
@@ -1,25 +1,28 @@
 return {
-  'HiPhish/rainbow-delimiters.nvim',
-  config = function()
-    local rainbow_delimiters = require('rainbow-delimiters')
-    vim.g.rainbow_delimiters = {
-      strategy = {
-        [''] = rainbow_delimiters.strategy['global'],
-        vim = rainbow_delimiters.strategy['local'],
-      },
-      query = {
-        [''] = 'rainbow-delimiters',
-        lua = 'rainbow-blocks',
-      },
-      highlight = {
-        'RainbowDelimiterYellow',
-        'RainbowDelimiterBlue',
-        'RainbowDelimiterOrange',
-        'RainbowDelimiterGreen',
-        'RainbowDelimiterViolet',
-        'RainbowDelimiterCyan',
-        'RainbowDelimiterRed',
-      },
-    }
-  end,
+  {
+    'HiPhish/rainbow-delimiters.nvim',
+    event = 'BufReadPre',
+    config = function()
+      local rainbow_delimiters = require('rainbow-delimiters')
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = rainbow_delimiters.strategy['global'],
+          vim = rainbow_delimiters.strategy['local'],
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+        },
+        highlight = {
+          'RainbowDelimiterYellow',
+          'RainbowDelimiterBlue',
+          'RainbowDelimiterOrange',
+          'RainbowDelimiterGreen',
+          'RainbowDelimiterViolet',
+          'RainbowDelimiterCyan',
+          'RainbowDelimiterRed',
+        },
+      }
+    end,
+  },
 }
