e468021e98e2afdbf998c9b54afdd92baaf407c1
Devin Sackett <dsac@hotmail.com>
2025-07-14 17:53:40 -0700

    Added rainbow-delimiters plugin

--- cnvim/init.lua
+++ cnvim/init.lua
@@ -3,15 +3,12 @@
 require('config').setup()
 
 -- look into built in completion
--- todo-comments
 -- c#
 -- typescript
 -- lua
 -- nu
 -- json / jsonc
--- remove bufferline for inline
 -- flash
 -- trouble
 -- treesitter
 -- session persistence
--- rainbowbrackets
