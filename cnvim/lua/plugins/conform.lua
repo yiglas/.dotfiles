41e906c2e94e9ce8a4688980afd265c7d6c80650
Devin Sackett <devinsackett@filevine.com>
2025-07-15 13:58:44 -0700

    Moved conform to its own plugin

---
+++ cnvim/lua/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>cf',
+        function()
+          require('conform').format({ async = true, lsp_format = 'fallback' })
+        end,
+        mode = '',
+        desc = 'Format buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function()
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end,
+    },
+  },
+}
